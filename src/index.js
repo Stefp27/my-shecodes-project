f8ba7762f1c9c2c44a958245ee311f68cc6aeca9
Stefp27 <stefania.p27@gmail.com>
2021-05-26 23:15:33 +0200

    Homework for week 5
    There is currently an error with Axios that I cannot fix.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,46 @@
-//Feature 1//
+//Display real time and date//
 
 let dateElement = document.querySelector("#date");
 
 let currentTime = new Date();
 let hours = currentTime.getHours();
-if (hours < 10) { hours = `0${hours}`}
+if (hours < 10) {
+  hours = `0${hours}`;
+}
 let minutes = currentTime.getMinutes();
-if (minutes < 10) { minutes = `0${minutes}`};
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
 let day = currentTime.getDay();
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
 
 dateElement.innerHTML = `${days[day]} ${hours}:${minutes}`;
 
-//Feature 2//
+//City updates as per the searched city//
 
-function search (event) {
-    event.preventDefault ();
-    let cityElement = document.querySelector("#city");
-    let cityInput = document.querySelector ("#city-input");
-    cityElement.innerHTML = cityInput.value;
-    cityElement.innerHTML = CityElement.toUpperCase();
+function displayWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#hum").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = response.data.wind.speed;
 }
+function search(event) {
+  event.preventDefault();
+  let apiKey = "ba0f360e4ade3504405f1836416cb716";
+  let city = document.querySelectorAll("city-input").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(displayWeather);
+}
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
-//Bonus feature //
